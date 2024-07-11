b24b8be2cc27ca696c3c0b3f992718c09437f1e4
Nolif1 <dawidos1505@wp.pl>
2024-07-11 15:08:18 +0200

    Update kic-resp.user.js

--- kic-resp.user.js
+++ kic-resp.user.js
@@ -1,13 +1,27 @@
+// ==UserScript==
+// @name         kic resp
+// @namespace    http://tampermonkey.net/
+// @version      1.4
+// @description  powiadomienie kica
+// @author       Nolifequ
+// @icon         https://cdn-icons-png.flaticon.com/512/523/523442.png
+// @match        https://fobos.margonem.pl/
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/Nolif1/kicownik/main/kic-resp.user.js
+// @downloadURL  https://raw.githubusercontent.com/Nolif1/kicownik/main/kic-resp.user.js
+// ==/UserScript==
+
 (function() {
     'use strict';
 
     const accessTokens = [
         'o.15n00HNr0a2cTae4bNX9B6mTh290U4eh',
-        'o.zPfR1mkUeCLjaChOxwC3P9CYAEUZLOjt',
+        //'o.zPfR1mkUeCLjaChOxwC3P9CYAEUZLOjt',
+        'token3',
     ];
 
     let notificationCount = 0;
-    const maxNotifications = 5;
+    const maxNotifications = 3;
 
     function sendPushbulletNotification(title, body) {
         const url = 'https://api.pushbullet.com/v2/pushes';
@@ -41,25 +55,21 @@
             return;
         }
 
-        const mobName = 'Królik';
-        const elements = document.querySelectorAll('*');
-        let found = false;
+        const targetImageURL = 'https://micc.garmory-cdn.cloud/obrazki/npc/dom/bestia64.gif';
+        const elements = document.querySelectorAll('img');
 
         elements.forEach(element => {
-            if (element.textContent.includes(mobName)) {
-                found = true;
+            if (element.src === targetImageURL) {
+                console.log('Znaleziono obrazek:', element);
+                sendPushbulletNotification('Zaginiony Zakonnik Astralny', 'Lokalizacja na mapie');
+                notificationCount++;
+
+                if (notificationCount >= maxNotifications) {
+                    clearInterval(checkInterval);
+                }
             }
         });
-
-        if (found) {
-            sendPushbulletNotification('Zabójczy Królik (tytan 70lvl)', 'Jaskinia Caerbannoga (29,17)');
-            notificationCount++;
-
-            if (notificationCount >= maxNotifications) {
-                clearInterval(checkInterval);
-            }
-        }
     }
 
-    const checkInterval = setInterval(checkForMob, 3000);
+    const checkInterval = setInterval(checkForMob, 1000);
 })();
