07de1fc863b6e9e3ceb4f1c3a0e8e733a909743b
Nolif1 <dawidos1505@wp.pl>
2024-07-11 14:49:31 +0200

    Update kic-resp.user.js

--- kic-resp.user.js
+++ kic-resp.user.js
@@ -1,1 +1,76 @@
+// ==UserScript==
+// @name         KICOWNIK
+// @namespace    http://tampermonkey.net/
+// @version      1.2
+// @description  Na NI trzeba mieć włączony wykrywacz
+// @author       Nolifequ
+// @icon         https://cdn-icons-png.flaticon.com/512/523/523442.png
+// @match        https://fobos.margonem.pl/
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+    const accessTokens = [
+        'o.15n00HNr0a2cTae4bNX9B6mTh290U4eh',
+        'o.zPfR1mkUeCLjaChOxwC3P9CYAEUZLOjt',
+    ];
+
+    let notificationCount = 0;
+    const maxNotifications = 5;
+
+    function sendPushbulletNotification(title, body) {
+        const url = 'https://api.pushbullet.com/v2/pushes';
+        const data = {
+            type: 'note',
+            title: title,
+            body: body,
+        };
+
+        accessTokens.forEach(token => {
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Access-Token': token,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('powiadomienie wysłane:', data);
+            })
+            .catch(error => {
+                console.error('błąd:', error);
+            });
+        });
+    }
+
+    function checkForMob() {
+        if (notificationCount >= maxNotifications) {
+            return;
+        }
+
+        const mobName = 'Zabójczy Królik';
+        const elements = document.querySelectorAll('*');
+        let found = false;
+
+        elements.forEach(element => {
+            if (element.textContent.includes(mobName)) {
+                found = true;
+            }
+        });
+
+        if (found) {
+            sendPushbulletNotification('Zabójczy Królik (tytan 70lvl)', 'Jaskinia Caerbannoga (29,17)');
+            notificationCount++;
+
+            if (notificationCount >= maxNotifications) {
+                clearInterval(checkInterval);
+            }
+        }
+    }
+
+    const checkInterval = setInterval(checkForMob, 3000);
+})();
